9e44037a1c42b1b00b5e6cd997be74431ba64285
Aditya Jain <adityajai2104@gmail.com>
2025-02-17 11:30:02 +0530

    Report Page added

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
     DATABASE_URL: process.env.DATABASE_URL,
     WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
     WEB3_AUTH_CLIENT_SECRET: process.env.WEB3_AUTH_CLIENT_SECRET,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
   },
 };
 
